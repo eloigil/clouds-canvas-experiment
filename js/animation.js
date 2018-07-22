3ce807ac774a40f16136cbc40afc107356130953
eloigil <eloigil6@gmail.com>
2018-07-22 20:17:07 +0200

    added class animation

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,21 @@
 
 class Animation {
   constructor (canvas, ctx) {
+    this.ctx = ctx;
+
     this.width = canvas.width;
     this.height = canvas.height;
+
+    this.renderEngine = new RenderEngine();
+
+    this.floor = null;
+    this.clouds = [];
+
+    this._start();
+  }
+
+  _start () {
+    this._createFloor();
+    this._update();
   }
 }
