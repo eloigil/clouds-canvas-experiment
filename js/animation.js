d41b123df280ab5e1ac77a1f846d2d55f28167af
eloigil <eloigil6@gmail.com>
2018-07-22 20:12:57 +0200

    initial commit

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+'use strict';
+
+class Animation {
+  constructor (canvas, ctx) {
+    this.width = canvas.width;
+    this.height = canvas.height;
+  }
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+function main () {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const animation = new Animation();
+}
+
+window.addEventListener('load', main);
